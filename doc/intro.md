f040efbbe4e00f961865745ee24d53bf7bf0dc72
Kenneth Leung <ccllnekk@gmail.com>
2018-04-12 23:11:03 -0700

    kl

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to xxx
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/test/elmo/core_test.clj
@@ -0,0 +1,7 @@
+(ns xxx.core-test
+  (:require [clojure.test :refer :all]
+            [xxx.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
